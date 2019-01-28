9e7e9ef641ff61a92b28dde66ca99afdb82c7861
Daniel THIRION <defvs.daniel@gmail.com>
2019-01-28 14:36:22 +0100

    Début du programme (base)

---
+++ Pong.vhd
@@ -0,0 +1,20 @@
+library ieee ;
+	use ieee.std_logic_1164.all;
+	use ieee.numeric_std.all;
+
+entity Pong is
+  port (
+    MAX10_CLK1_50 : in std_logic;
+    
+  ) ;
+end Pong ;
+
+architecture work of Pong is
+
+
+
+begin
+
+
+
+end architecture ; -- work
