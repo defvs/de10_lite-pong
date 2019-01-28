2091b1a3309f04bb6116a3985fbb57c1fdc31f08
Daniel THIRION <defvs.daniel@gmail.com>
2019-01-28 17:39:50 +0100

    Renommage des processus et indentation du code

--- Pong.vhd
+++ Pong.vhd
@@ -3,70 +3,54 @@ library ieee ;
 	use ieee.numeric_std.all;
 
 entity Pong is port (
-MAX10_CLK1_50 : in std_logic ;
-VGA_HS : buffer std_logic;
-VGA_VS : buffer std_logic;
-GPIO: buffer std_logic_vector (1 downto 0)
-) ;
+	MAX10_CLK1_50 : in std_logic;
+	VGA_HS : buffer std_logic;
+	VGA_VS : buffer std_logic;
+	GPIO: buffer std_logic_vector (1 downto 0)
+);
 end Pong ;
 
 architecture work of Pong is
-signal VGA_h: std_logic;
-signal compteur_h: integer range 0 to 799;
-signal compteur_v: integer range 0 to 524;
+	signal VGA_h: std_logic;
+	signal compteur_h: integer range 0 to 799;
+	signal compteur_v: integer range 0 to 524;
 begin
-
-
-hor:process(MAX10_CLK1_50)
+	diviseur_vga :process(MAX10_CLK1_50)
 	begin
-	if rising_edge(MAX10_CLK1_50) then VGA_h <= not VGA_h;
-	end if;
-end process hor;
+		if rising_edge(MAX10_CLK1_50) then 
+			VGA_h <= not VGA_h;
+		end if;
+	end process diviseur_vga;
 
+	synchro_h : process(VGA_h)
+	begin
+		if rising_edge(VGA_h) then
+			compteur_h <= compteur_h + 1;
 
-ligne:process(VGA_h)
-begin
-	if rising_edge(VGA_h) then
-		compteur_h <= compteur_h + 1;
-			
-		CASE (compteur_h) IS
-			when 703 => VGA_HS <='0'; 
-			when 0 => VGA_HS <= '1';
-			when 799 => compteur_h <= 0;
-			when others =>
-		end case;
-	end if;
+			case (compteur_h) is
+				when 703 => VGA_HS <='0'; 
+				when 0 => VGA_HS <= '1';
+				when 799 => compteur_h <= 0;
+				when others =>
+			end case;
+		end if;
+	end process synchro_h;
 
-end process ligne;
+	synchro_v : process(VGA_HS)
+	begin 
+		if rising_edge(VGA_HS) then 
+			compteur_v <= compteur_v +1;
 
-compteB:process(VGA_HS)
-begin 
-	if rising_edge(VGA_HS) then 
-		compteur_v <= compteur_v +1;
-		case (compteur_v) is 
-			when 0 => VGA_VS <='1';
-			when 522 => VGA_VS <='0';
-			when 524 => compteur_v <= 0;
-			when others =>
-		end case;
-	end if;
-end process compteB;
+			case (compteur_v) is 
+				when 0 => VGA_VS <='1';
+				when 522 => VGA_VS <='0';
+				when 524 => compteur_v <= 0;
+				when others =>
+			end case;
+		end if;
+	end process synchro_v;
 
-GPIO(0) <= VGA_HS;
-GPIO(1) <= VGA_VS;
+	GPIO(0) <= VGA_HS;
+	GPIO(1) <= VGA_VS;
 
 end architecture ;
-	
-	
-	
-	--if compteur = 703 then
-		--VGA_HS <= '0';
-	--elsif compteur = 0 then VGA_HS <= '1';
-	--end if;
-	
-	
-	--if compte = 0 then 
-		--VGA_VS <='1';
-	--elsif compte = 522 then 
-		--VGA_VS <='0';
-	--end if ;
