91ddb97be824a7f5a4c10864877c1d448fa59cfe
Daniel THIRION <defvs.daniel@gmail.com>
2019-01-30 11:47:38 +0100

    Système de score

--- Pong.vhd
+++ Pong.vhd
@@ -13,6 +13,9 @@ entity Pong is
     VGA_B : buffer integer range 0 to 15;
 
     SW : in std_logic_vector(9 downto 0);
+    HEX5 : out std_logic_vector(0 to 6);
+    HEX0 : out std_logic_vector(0 to 6);
+
     LEDR : out std_logic_vector(1 downto 0);
     GPIO : out std_logic_vector(1 downto 0)
   ) ;
@@ -40,7 +43,7 @@ architecture work of Pong is
     alias rSW is SW(0); -- et droite
 
     signal gameTick : std_logic; -- Horloge du timing du jeux
-    signal tickCounter : integer range 0 to 250000; -- Diviseur pour l'horloge du jeu, 20Hz
+    signal tickCounter : integer range 0 to 250000; -- Diviseur pour l'horloge du jeu, 100Hz
 
     type direction is (l_u, l_d, r_u, r_d);
     -- l = vers la droite // r = vers la gauche // u = vers le haut // d = vers le bas
@@ -48,6 +51,10 @@ architecture work of Pong is
     -- Position de la balle (point le plus en haut à gauche de celle-ci)
     signal ball_x : integer range 0 to 639 := 20; -- Position en x (colonnes)
     signal ball_y : integer range 0 to 479 := 300; -- Position en y (lignes)
+
+    signal lScore : integer range 0 to 10;
+    signal rScore : integer range 0 to 10;
+    -- signal pause : std_logic := '0';
 
 begin
     VGA_Divider : process( clk ) -- 25MHz
@@ -187,7 +194,7 @@ begin
         if rising_edge(gameTick) then
             if (ballDir = l_u) or (ballDir = l_d) then
                 ball_x <= ball_x - 1;
-                if ball_x = 17 then
+                if ball_x <= 17 and (ball_y <= (lPalet + 128) and ball_y >= lPalet) then
                     case( ballDir ) is
                         when l_d => ballDir <= r_d;
                         when l_u => ballDir <= r_u;
@@ -197,7 +204,7 @@ begin
             end if ;
             if (ballDir = r_u) or (ballDir = r_d) then
                 ball_x <= ball_x + 1;
-                if ball_x = (480 - 17 - 16) then
+                if (ball_x = (639 - 17 - 16)) and (ball_y <= (rPalet + 128) and ball_y >= rPalet) then
                     case( ballDir ) is
                         when r_d => ballDir <= l_d;
                         when r_u => ballDir <= l_u;
@@ -225,8 +232,80 @@ begin
                     end case ;
                 end if ;
             end if ;
+            if ball_x = 0 then
+                ballDir <= r_u;
+                ball_x <= 20;
+                ball_y <= 300;
+                rScore <= rScore + 1;
+            end if ;
+            if ball_x = (639 - 16) then
+                ballDir <= l_u;
+                ball_x <= 619;
+                ball_y <= 300;
+                lScore <= lScore + 1;
+            end if ;
         end if ;
     end process ; -- ball_movement
+
+    -- game_end : process( ball_x )
+    -- begin
+    -- end process ; -- game_end
+
+    score_print : process( lScore, rScore )
+    begin
+        case( lScore ) is
+            when 0 =>
+                HEX5 <= not "1111110";
+            when 1 =>
+                HEX5 <= not "0110000";
+            when 2 =>
+                HEX5 <= not "1101101";
+            when 3 =>
+                HEX5 <= not "1111001";
+            when 4 =>
+                HEX5 <= not "0110011";
+            when 5 =>
+                HEX5 <= not "1011011";
+            when 6 =>
+                HEX5 <= not "1011111";
+            when 7 =>
+                HEX5 <= not "1110000";
+            when 8 =>
+                HEX5 <= (others => '0');
+            when 9 =>
+                HEX5 <= not "1111011";
+            when 10 =>
+                HEX5 <= not "1100010";
+                HEX0 <= not "0001000";
+                -- pause <= '1';
+        end case ;
+        case( rScore ) is
+            when 0 =>
+                HEX0 <= not "1111110";
+            when 1 =>
+                HEX0 <= not "0110000";
+            when 2 =>
+                HEX0 <= not "1101101";
+            when 3 =>
+                HEX0 <= not "1111001";
+            when 4 =>
+                HEX0 <= not "0110011";
+            when 5 =>
+                HEX0 <= not "1011011";
+            when 6 =>
+                HEX0 <= not "1011111";
+            when 7 =>
+                HEX0 <= not "1110000";
+            when 8 =>
+                HEX0 <= (others => '0');
+            when 9 =>
+                HEX0 <= not "1111011";
+            when 10 =>
+                HEX0 <= not "1100010";
+                HEX5 <= not "0001000";
+                -- pause <= '1';
+        end case ;
+    end process ; -- score_print
 
     -- Debug Signaux
     LEDR(0) <= VGA_HS;
