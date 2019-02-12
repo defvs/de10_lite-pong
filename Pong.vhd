601b09959edea2bd35d5208815fe9cd446606770
Loic Savornin <loic.savornin@yahoo.fr>
2019-02-12 16:00:43 +0100

    Dernier commit tout marche

--- Pong.vhd
+++ Pong.vhd
@@ -7,7 +7,7 @@ entity Pong is port (
 	VGA_HS : buffer std_logic;
 	VGA_VS : buffer std_logic;
 	GPIO: buffer std_logic_vector (1 downto 0);
-	SW : in std_logic_vector (2 downto 0);
+	SW : in std_logic_vector (9 downto 0);
 	VGA_R : out integer range 0 to 15;
 	VGA_G : out integer range 0 to 15;
 	VGA_B : out integer range 0 to 15
@@ -20,14 +20,32 @@ architecture work of Pong is
 	signal compteur_v: integer range 0 to 524;
 	signal video_h : std_logic;
 	signal video_v : std_logic;
-	constant size : integer := 32;
-	signal x : integer range 0 to 799;
+	constant size : integer := 20;
+	constant largeurac : integer := 10;
+	constant longueurac : integer := 125;
+	signal yracg : integer range 0 to 524;
+	signal yracd : integer range 0 to 524;
+	signal x : integer range 0 to 799; 
 	signal y : integer range 0 to 524;
 	signal vitessX : integer range -1 to 1 :=1;
 	signal vitessY : integer range -1 to 1 :=1;
 	signal vitesse : std_logic;
+	signal horloge : std_logic;
+	signal diviseur : integer range 0 to 25000000;
+	signal acceleration : integer range 0 to 25000000 :=250000;
 begin
 	
+	horlogge :process(MAX10_CLK1_50)
+	begin
+		if rising_edge(MAX10_CLK1_50) then 
+			diviseur <= diviseur+1;
+	
+			if diviseur = acceleration then
+				horloge <= not horloge;
+				diviseur <= 0;
+			end if;
+		end if;
+	end process horlogge;
 	
 	diviseur_vga :process(MAX10_CLK1_50)
 	begin
@@ -76,39 +94,132 @@ begin
 		end if;
 	end process synchro_v;
 	
-	vite : process(VGA_VS)
+	vite : process(horloge)
 	begin 
-		if rising_edge(VGA_VS) then 
+		if rising_edge(horloge) then 
 			vitesse <= not vitesse;
 		end if;
 	end process vite;
 	
 	
-	bande : process(compteur_h,compteur_v,vitesse)
+	balle : process(compteur_h,compteur_v,vitesse,SW)
 	begin
 		if rising_edge(vitesse) then 
 			x <= x+vitessx;
 			y <= y+vitessy;
+			
+			if SW(9) = '0' then
+				if (yracg < 509-longueurac) then
+					yracg <= yracg + 1;
+				end if;
+			else
+				if (yracg > 30) then
+					yracg <= yracg - 1;
+				end if;
+			end if;
+			
+			if SW(0) = '0' then
+				if (yracd < 509-longueurac) then
+					yracd <= yracd + 1;
+				end if;
+			else
+				if (yracd > 30) then
+					yracd <= yracd - 1;
+				end if;
+			end if;
+			
+			
+		
+		
+			if x = 683-size-largeurac then 
+				if y >= yracd and y <= yracd+longueurac then
+					vitessx <= -1;
+					acceleration <= acceleration-8000;
+				else 
+					x <= 340;
+					y <= 250;
+				end if;
+			end if ;
+			
+			if x = 44+largeurac then 
+				if y >= yracg and y <= yracg+longueurac then
+					vitessx <= +1;
+					acceleration <= acceleration-8000;
+				else
+					x <= 340;
+					y <= 250;
+				end if;
+			end if ;
+			
+			if y = 509-size then 
+				vitessy <= -1;
+			end if;
+			if y = 30 then
+				vitessy <=+1;
+			end if;
 		end if;
+	
 			
 		if video_h = '1' and video_v = '1' then 
 			
-			if compteur_v >= y and compteur_v <= y+size and compteur_h >= x and compteur_h <= x+size then
+			
+			
+			if SW(3) = '1' then
 				VGA_R <= 15;
 			else 
-				VGA_R <= 0;
+				VGA_R <=0;
 			end if;
+			if SW(1) = '1' then
+				VGA_B <= 15;
+			else 
+				VGA_B <= 0;
+			end if;
+			if SW(2) = '1' then
+				VGA_G <= 15;
+			else 
+				VGA_G <= 0;
+			end if;
+	
+			
+			
+			
+			if compteur_v >= y and compteur_v <= y+size and compteur_h >= x and compteur_h <= x+size then
+				VGA_R <= 15;
+				VGA_G <= 0;
+				VGA_B <= 0;
+		
+			end if;
+			
+			
+			if compteur_v >= yracg and compteur_v <= yracg+longueurac and compteur_h <= largeurac+44 then
+				VGA_R <= 1;
+				VGA_G <= 11;
+				VGA_B <= 6;
+		
+			end if;
+			
+			
+			
+			
+			if compteur_v >= yracd and compteur_v <= yracd+longueurac and compteur_h >= 683-largeurac and compteur_h <= 683 then
+				VGA_R <= 0;
+				VGA_G <= 6;
+				VGA_B <= 7;
+		
+			end if;
+			
+			
+			
+		
 		else
 			VGA_R <= 0;
+			
+			
 			VGA_B <= 0;
+			
 			VGA_G <= 0;
 		
 		end if;
-		
-	
-		
-		
-		
 		
 		
 		--if compteur_v <= 400 and compteur_v >= 200 and compteur_h >= 400 and compteur_h <= 600 then
@@ -116,23 +227,18 @@ begin
 		--else
 	--		VGA_G <=0;
 	--	end if;
-	end process bande;
+	end process balle;
 	
-	test : process(x,y)
-	begin
-		if x = 683-size then 
-			vitessx <= -1;
-		end if ;
-		if x = 44 then
-			vitessx <= +1;
-		end if;
-		if y = 509-size then 
-			vitessy <= -1;
-		end if;
-		if y = 30 then
-			vitessy <=+1;
-		end if;
-	end process test;
+	--test : process(x,y)
+	--begin
+	
+		
+		
+		
+		
+		
+		
+	--end process test;
 	
 	
 	
@@ -154,14 +260,9 @@ begin
 	
 	
 	
---	golli : process(video_h,video_v)
+--	golli : process(SW)
 --	begin
 --		if video_h = '1' and video_v = '1' then 
---			if SW(0) = '1' then
---				VGA_R <= 15;
---			else 
---				VGA_R <= 0;
---			end if ;
 --			if SW(1) = '1' then
 --				VGA_B <= 15;
 --			else 
@@ -172,8 +273,7 @@ begin
 --			else 
 --				VGA_G <= 0;
 --			end if;
---		else
---			VGA_R <= 0;
+--		else 
 --			VGA_B <= 0;
 --			VGA_G <= 0;
 --		end if;
